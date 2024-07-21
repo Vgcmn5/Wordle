facb36fb1422702adb96fa23aea2c84778cca08c
Vgcmn5 <95554517+Vgcmn5@users.noreply.github.com>
2024-07-21 08:04:51 -0500

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,73 @@
+// TO DO:
+// - Emoji output like the real Wordle
+// - Give a warning if there are not very many words of the specified length in the dictionary
+
+// KNOWN ISSUES:
+// - If the word is guessed correctly on the FIRST try, it will not turn green
+import java.util.*;
+class Main {
+  public static void main(String[] args) throws InterruptedException {
+    
+    Scanner scan = new Scanner(System.in);
+    int wordLength = 5; // default: 5
+    int numGuesses = 6; // default: 6
+
+    if (wordLength > 29) {
+      wordLength = 5; // reset to default if word length is too long
+      System.out.println(Text.YELLOW + "Warning: Word length is too long! Defaulting to 5." + Text.RESET);
+    }
+
+    System.out.println(Text.CLEAR + "Wordle: You have 6 tries to guess the " + wordLength + " letter word!\n");
+    SpellChecker checker = new SpellChecker(wordLength);
+
+    String word = checker.getWordleAnswer();
+    String alphabet = "QWERTYUIOP\nASDFGHJKL\n ZXCVBNM\n";
+    String[] allGuesses = new String[numGuesses];
+    String result = "";
+
+    // guess loop
+    int guessIndex;
+    for (guessIndex = 0; guessIndex < numGuesses; guessIndex++) {
+      showHints(allGuesses, alphabet, guessIndex);
+      System.out.print((guessIndex+1) + "/" + numGuesses + ": ");
+      String guess = scan.nextLine().toUpperCase();
+      if (!guess.matches("[A-Z]{"+word.length()+"}") || !checker.isWord(guess)) {
+        guessIndex++;
+        showHints(allGuesses, alphabet, guessIndex);
+        Text.showError("Guess must be a real " + word.length() + " letter word\n");
+        Thread.sleep(1000); // waits 1000 milliseconds (1 second)
+        guessIndex-=2;  continue; // returns to the same guess index after error
+      }
+      
+      // guess is compared to wordle answer
+      allGuesses[guessIndex] = checker.analyzeGuess(guess, alphabet)[0];
+      alphabet = checker.analyzeGuess(guess, alphabet)[1];
+      if (guess.equals(word)) {
+        showHints(allGuesses, alphabet, guessIndex);
+        System.out.print("\nCongratulations!\n\nGuesses: " + (guessIndex+1) + "/" + numGuesses);
+        break;
+      } else if (guessIndex+1 == numGuesses) {
+        showHints(allGuesses, alphabet, guessIndex);
+        System.out.print("You ran out of guesses!\nThe word is " + word);
+      }
+      
+    } // end of guess loop
+    
+    scan.close();
+  } // end of main method
+
+  // show updated hints
+  private static void showHints(String[] hints, String alphabet ,int guessIndex) {
+    if (guessIndex > 0) {
+      System.out.print(Text.CLEAR);
+      System.out.println(alphabet);
+      for (String s : hints) {
+        if (s != null) {
+          System.out.println(s);
+        }
+      }
+    }
+  }
+  
+  
+}
---
+++ SpellChecker.java
@@ -0,0 +1,118 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpellChecker {
+    private String[] dictionary;
+    private String[] wordleList;
+    private String wordleAnswer;
+  
+    public String[] getDictionary() { return dictionary; }
+    public String getWordleAnswer() { return wordleAnswer; }
+
+    public SpellChecker(int wordLength) {
+      try {
+        dictionary = readLines("Words/dictionary.txt");
+        wordleList = readLines("Words/WordleList.txt");
+        if (wordLength == 5){
+          // pick a random 5 letter word from the wordle answer list
+          wordleAnswer = wordleList[(int)(Math.random() * wordleList.length)].toUpperCase();
+        } else {
+          // filter to only words of the specified length
+          List<String> filteredList = new ArrayList<>();
+          for (String word : dictionary) {
+            if (word.length() == wordLength) {
+              filteredList.add(word);
+            }
+          }
+          wordleAnswer = filteredList.get((int)(Math.random() * filteredList.size())).toUpperCase(); // get answer?
+        }
+        //System.out.println("the ANSWER is: " + wordleAnswer);
+      } catch (IOException e) {
+        // print out the exception that occurred
+        System.out.println("Unable to access "+e.getMessage());
+      }
+      
+    }
+
+    // check if input is a real english word
+    public boolean isWord(String word) {
+      for (String str: dictionary) {
+        if (str.equalsIgnoreCase(word)) { return true; }
+      }
+      return false;
+    }
+
+    // analyze wordle guess by comparing it to the wordle answer
+    public String[] analyzeGuess(String guess, String alphabet) {
+      String result = "";
+      for (int charIndex = 0; charIndex < wordleAnswer.length(); charIndex++) {
+        char wordChar = wordleAnswer.charAt(charIndex);
+        char guessChar = guess.charAt(charIndex);
+        // if the character is guessed correctly, it turns green
+        if (guessChar == wordChar) {
+          result = result + Text.GREEN + wordChar + Text.RESET;
+          alphabet = alphabet.replace(wordChar+"", Text.GREEN + wordChar + Text.RESET);
+        }
+        // if the character is in the word somewhere it wasn't guessed, it turns yellow
+        else if (wordleAnswer.contains(guessChar+"") && !guessedAll(guessChar,guess)) {
+          result = result + Text.YELLOW + guessChar + Text.RESET;
+          if (!alphabet.contains(Text.GREEN + guessChar)) {
+            alphabet = alphabet.replace(guessChar+"", Text.YELLOW + guessChar + Text.RESET);
+          }
+        }
+        // else: the character is not in the word
+        else {
+          result = result + guessChar;
+          if (!alphabet.contains(Text.GREEN + guessChar)) {
+          alphabet = alphabet.replace(guessChar+"",Text.BLACK + guessChar + Text.RESET);
+          }
+        }
+      }
+      return new String[]{result,alphabet};
+    }
+
+    // count the number of times a character appears in a String
+    private int countChar(char chr, String str) {
+      int count = 0;
+      for (int i=0; i<str.length(); i++) {
+        if (str.charAt(i) == chr) {
+          count++;
+        }
+      }
+      return count;
+    }
+
+    // check if all occurrences of a specific character have been correctly guessed
+    private boolean guessedAll(char chr, String guess) {
+      int numGuessed = 0;
+      for (int i=0; i<guess.length(); i++) {
+        if (guess.charAt(i) == chr && guess.charAt(i) == wordleAnswer.charAt(i)) {
+          numGuessed++;
+        }
+      }
+      return numGuessed == countChar(chr, wordleAnswer);
+    }
+
+
+
+    public static String[] readLines(String filename) throws IOException {
+      FileReader fileReader = new FileReader(filename);
+         
+      BufferedReader bufferedReader = new BufferedReader(fileReader);
+      List<String> lines = new ArrayList<String>();
+      String line = null;
+         
+      while ((line = bufferedReader.readLine()) != null) {
+        //if (line.length() == 5) { 
+          lines.add(line); //}
+      }
+         
+      bufferedReader.close();
+      return lines.toArray(new String[lines.size()]);
+    }
+      
+    
+  }
